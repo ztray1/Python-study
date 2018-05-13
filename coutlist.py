a726332b192841d188bf7b8195fe7a54c2818d75
ztray1 <zhangtuo332@gmail.com>
2018-05-13 12:23:42 -0400

    Python

---
+++ coutlist.py
@@ -0,0 +1,16 @@
+import math
+def get_primes(input_list):
+    return (element for element in input_list if is_prime(element))
+ 
+ 
+def is_prime(number):
+    if number > 1:
+        if number == 2:
+            return True
+        if number % 2 == 0:
+            return False
+        for current in range(3, int(math.sqrt(number) + 1), 2):
+            if number % current == 0:
+                return False
+        return True
+    return False
